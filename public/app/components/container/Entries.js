c2ae8b0e0be88bf0de0a460af7fa282632e349d8
panforrest <guoqianp@gmail.com>
2016-09-02 12:15:33 -0400

    APIManagerWorkPriorRedux

--- public/app/components/container/Entries.js
+++ public/app/components/container/Entries.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react'
 import EntryPreview from '../../components/EntryPreview'
+import APIManager from '../../../utils/APIManager'
 
 class Entries extends Component {
     constructor(props, context){
     	super(props, context)
     	this.state = {
             entries: [
-                {id:0, title:'My next project', url:'http://www.herePong.com'},
-                {id:1, title:'My following project', url:'http://www.bootCamper.com'}    
+                // {id:0, title:'My next project', url:'http://www.herePong.com'},
+                // {id:1, title:'My following project', url:'http://www.bootCamper.com'}    
             ]
     	}
+    }
+
+    componentDidMount(){
+        // console.log('componentDidMount: ')
+        var _this = this
+        APIManager.handleGet('/api/entry', null, function(err, response){
+            if (err) {
+                alert(err.message)
+                return
+            }
+
+            console.log('componentDidMount: '+JSON.stringify(response.results))
+            var results = response.results
+            _this.setState({
+                entries: results
+            })
+        })
     }
 
     render() {
