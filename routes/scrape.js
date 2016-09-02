6b7f4d30096e24ceedfded2f49aec183a96bf2b1
panforrest <guoqianp@gmail.com>
2016-09-02 18:55:33 -0400

    jsonRenderedOnWebPage

--- routes/scrape.js
+++ routes/scrape.js
@@ -7,10 +7,6 @@ var cheerio = require('cheerio')
 router.get('/:url', function(req, res, next) {
 	var url = req.params.url
 
-	// res.json({
-	// 	confirmation: 'success',
-	// 	url: url
-	// })
 
     superagent
     .get('https://www.youtube.com/watch?v=-1GJQ3c94jQ&feature=youtu.be')
@@ -18,16 +14,31 @@ router.get('/:url', function(req, res, next) {
     .set('Accept', 'text/html')
     .end(function(err, response){
     	if (err) {
-    		alert(err)
     		return
     	}
 
         // res.json(response.text)
+        var props = ['og:title', 'og:description', 'og:image']
+        var metaData = {}
         $ = cheerio.load(response.text)
         $('meta').each(function(i, meta){
-        	console.log('META: '+i, meta)
+            if (meta.attribs != null){
+            	var attribs = meta.attribs
+            	if (attribs.property != null) {
+            		var prop = attribs.property
+            		if (props.indexOf(prop) != -1){
+
+            			var key = prop.replace('og:', '')
+            			metaData[key] = attribs.content
+            		} 
+            	}
+            }
         })
+
+        console.log(JSON.stringify(metaData))
+        res.json(metaData)
     })
+
   
 });
 
