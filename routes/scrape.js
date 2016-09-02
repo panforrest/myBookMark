35ae2b2e2138946f512cf113606afba399ae3c2b
panforrest <guoqianp@gmail.com>
2016-09-02 18:15:33 -0400

    localhost3000|scrape|whatever_renderYoutubePage

--- routes/scrape.js
+++ routes/scrape.js
@@ -1,14 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var superagent = require('superagent')
 
 /* GET users listing. */
 router.get('/:url', function(req, res, next) {
 	var url = req.params.url
 
-	res.json({
-		confirmation: 'success',
-		url: url
-	})
+	// res.json({
+	// 	confirmation: 'success',
+	// 	url: url
+	// })
+
+    superagent
+    .get('https://www.youtube.com/watch?v=-1GJQ3c94jQ&feature=youtu.be')
+    .query(null)
+    .set('Accept', 'text/html')
+    .end(function(err, response){
+    	if (err) {
+    		alert(err)
+    		return
+    	}
+
+        res.json(response.text)
+    })
   
 });
 
